585130d71299f216ffc276f274c98cb1123f4d0d
Khachatur <115093476+KhachaturKhojoyan@users.noreply.github.com>
2026-07-04 20:21:34 +0400

    Some corrections

--- dijkstra.js
+++ dijkstra.js
@@ -67,30 +67,44 @@ function dijkstra(graph, start, end) {
 }
 
 const flights = [
-  { from: "Yerevan", to: "Moscow",   time: 2,  price: 200 },
-  { from: "Yerevan", to: "Paris",    time: 5,  price: 500 },
+  
+  { from: "Yerevan",  to: "Moscow",    time: 2.5, price: 220 },
+  { from: "Yerevan",  to: "Istanbul",  time: 2,   price: 180 },
+  { from: "Yerevan",  to: "Paris",     time: 5,   price: 500 },
+  { from: "Yerevan",  to: "Dubai",     time: 3,   price: 260 },
+  { from: "Yerevan",  to: "Belgrade",  time: 2.5, price: 190 },
 
-  { from: "Moscow",  to: "Berlin",   time: 4,  price: 300 },
-  { from: "Moscow",  to: "Istanbul", time: 3,  price: 250 },
+  
+  { from: "Moscow",   to: "Berlin",    time: 3,   price: 280 },
+  { from: "Moscow",   to: "Istanbul",  time: 3,   price: 250 },
 
-  { from: "Berlin",  to: "Paris",    time: 2,  price: 170 },
-  { from: "Berlin",  to: "London",   time: 2,  price: 180 },
+  { from: "Belgrade", to: "Berlin",    time: 2,   price: 170 },
+  { from: "Belgrade", to: "Rome",      time: 1.5, price: 140 },
+  { from: "Belgrade", to: "Paris",     time: 2.5, price: 180 },
 
-  { from: "Paris",   to: "London",   time: 1,  price: 120 },
-  { from: "Paris",   to: "Rome",     time: 2,  price: 160 },
+  
+  { from: "Berlin",   to: "Paris",     time: 2,   price: 170 },
+  { from: "Berlin",   to: "London",    time: 2,   price: 180 },
+  { from: "Berlin",   to: "Rome",      time: 2,   price: 160 },
 
-  { from: "Rome",    to: "Madrid",   time: 2,  price: 140 },
+  { from: "Paris",    to: "London",    time: 1,   price: 120 },
+  { from: "Paris",    to: "Rome",      time: 2,   price: 160 },
+  { from: "Paris",    to: "Madrid",    time: 2,   price: 150 },
 
-  { from: "Istanbul",to: "Dubai",    time: 4,  price: 350 },
+  { from: "Rome",     to: "Madrid",    time: 2,   price: 140 },
 
-  { from: "Dubai",   to: "New York", time: 14, price: 900 },
+  
+  { from: "Istanbul", to: "Dubai",     time: 4,   price: 350 },
 
-  { from: "London",  to: "New York", time: 7,  price: 600 },
-
-  { from: "Madrid",  to: "New York", time: 8,  price: 650 },
+  
+  { from: "London",   to: "New York",  time: 7,   price: 600 },
+  { from: "Madrid",   to: "New York",  time: 8,   price: 650 },
+  { from: "Paris",    to: "New York",  time: 8,   price: 700 },
+  { from: "Dubai",    to: "New York",  time: 14,  price: 900 }
 ];
 
 
+//test
 const graphByTime  = buildGraph(flights, "time");
 const graphByPrice = buildGraph(flights, "price");
 
